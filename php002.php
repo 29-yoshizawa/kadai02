ef2990709f36ccbb4f684261fbf79c869a0fccd4
aoi <ruriirono-hanaba-6ttun-inti@docomo.ne.jp>
2020-07-25 13:31:56 +0000

    kadai 7/25

--- php002.php
+++ php002.php
@@ -1,9 +1,16 @@
 <?php
-$name = 吉澤;
-if ($name = 吉澤) {
+$name = "吉澤";
+if ($name = "吉澤") {
     echo "私は吉澤です。";
 } else {
     echo "吉澤ではありません。";
 }
+"\n"
 
 
+$total = 0;
+for ($i = 0; $i <= 100; $i++) {
+  $total += $i;
+}
+echo $total;
+?>
---
+++ php_03.php
@@ -0,0 +1,29 @@
+<?php
+$name = "吉澤";
+if ($name = "吉澤") {
+    echo "私は吉澤です。";
+} else {
+    echo "吉澤ではありません。";
+}
+echo "\n";
+
+for($i = 1; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+$fruits = array("apple", "orange", "lemon", "blueberry", "melon");
+foreach($fruits as $fruits){
+echo "ひとつは" . $fruits;
+echo "\n";
+}
+
+$start = 1;
+$end = 100;
+for($i = $start; $i <= $end; $i++){
+    if ($i % 5 == 0) {
+        echo $i;
+        echo "\n";
+    }
+}
